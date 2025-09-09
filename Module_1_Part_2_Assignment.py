bae30323009de7dcacad0c08cbb5fb916b950591
Nathan Makowski <njm12@illinois.edu>
2025-09-09 01:10:30 +0000

    Created first function

--- Module_1_Part_2_Assignment.py
+++ Module_1_Part_2_Assignment.py
@@ -3,3 +3,14 @@
 This script requests a temperature value in Fahrenheit from the user,
 converts it to Celsius and Kelvin, and prints the results.
 """
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius.
+
+    Args:
+        fahrenheit (float): Temperature in degrees Fahrenheit.
+
+    Returns:
+        float: Temperature in degrees Celsius.
+    """
+    return (fahrenheit - 32) * 5.0 / 9.0
+
